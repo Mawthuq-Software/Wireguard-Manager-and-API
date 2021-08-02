7df99e31c4031b705b9cc8c5d6238270d5bc22b3
Raspberry Tech <noobie.mine@gmail.com>
2021-08-02 14:53:38 +0100

    added db and network

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/vishvananda/netlink v1.1.0
 	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20210506160403-92e472f520a5 // indirect
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.21.12
 )
--- main.go
+++ main.go
@@ -5,20 +5,27 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"gitlab.com/raspberry.tech/wireguard-manager-and-api/src/db"
 	"gitlab.com/raspberry.tech/wireguard-manager-and-api/src/logger"
+	"gitlab.com/raspberry.tech/wireguard-manager-and-api/src/network"
 )
 
 func main() {
 	fmt.Println("WG MANAGER AND API STARTING UP")
 
-	fmt.Println("Env file loading - 1/")
+	fmt.Println("Env file loading - 1/6")
 	errEnv := godotenv.Load(".env")
 	if errEnv != nil {
 		fmt.Println("Env failed to load - FAILED")
 		os.Exit(1)
 	}
 
-	fmt.Println("Logger starting up - 2/")
+	fmt.Println("Logger starting up - 2/6")
 	logger.LoggerSetup()
 
+	fmt.Println("Starting database - 3/6")
+	db.DBStart()
+
+	fmt.Println("Starting of network - 4/6")
+	network.SetupWG()
 }
