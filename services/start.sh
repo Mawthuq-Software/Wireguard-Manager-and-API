b908abdc9c05f1f55450af097a114372e13f1cf7
Raspberry Tech <noobie.mine@gmail.com>
2021-08-04 10:35:58 +0200

    fixed docker image

--- services/start.sh
+++ services/start.sh
@@ -3,5 +3,4 @@ sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i wg0 -j ACCEPT;
 sudo systemctl start coredns
 sudo systemctl enable coredns
-ls -a ./
 sudo ./main
