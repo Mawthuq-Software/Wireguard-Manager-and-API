56284d9bf7007cdb9686742d5a983bb6d3fb7796
Raspberry Tech <noobie.mine@gmail.com>
2021-08-04 10:30:52 +0200

    test

--- services/start.sh
+++ services/start.sh
@@ -3,5 +3,5 @@ sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i wg0 -j ACCEPT;
 sudo systemctl start coredns
 sudo systemctl enable coredns
-ls -a ../
-sudo ../main
+ls -a ./
+sudo ./main
