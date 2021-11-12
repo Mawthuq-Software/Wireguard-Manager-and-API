7e1e67208118946014bdc02472f1ea06f7487613
Raspberry Tech <noobie.mine@gmail.com>
2021-11-12 16:46:01 +0100

    set defaults

--- src/api/router/router.go
+++ src/api/router/router.go
@@ -8,7 +8,7 @@ func NewRouter() *mux.Router {
 
 	manager := router.PathPrefix("/manager").Subrouter() //main subrouter
 
-	keys := manager.PathPrefix("/keys").Subrouter()         //specific subrouter
+	keys := manager.PathPrefix("/key").Subrouter()          //specific subrouter
 	keys.HandleFunc("", keyCreate).Methods("POST")          //post route for adding keys
 	keys.HandleFunc("", keyRemove).Methods("DELETE")        //delete route for removing keys
 	keys.HandleFunc("/enable", keyEnable).Methods("POST")   //post route for enabling key
