8813c54124d95a9a6a06200df84fa9a1accff8a5
Raspberry Tech <noobie.mine@gmail.com>
2021-12-18 16:42:36 +0000

    Tested and seems to be working.

--- src/config/config.go
+++ src/config/config.go
@@ -14,6 +14,7 @@ func LoadConfig() {
 	viper.SetDefault("SERVER.SECURITY", true)                        //SECURITY
 	viper.SetDefault("SERVER.AUTH", "ABCDEFG")                       //AUTHORISATION
 	viper.SetDefault("INSTANCE.IP.GLOBAL.ALLOWED", "0.0.0.0/0, ::0") //ALLOWED_IPs wireguard
+	viper.SetDefault("INSTANCE.PORT", 51820)
 
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
